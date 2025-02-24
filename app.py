7b22507be01726f280e3994138773c603eb55f70
indura-ER <indura_vidyasekara@engagerocket.co>
2025-02-25 02:05:46 +0530

    init commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
+from controls.video_stream import video_feed
+from controls.steering_control import control_steering
+from controls.throttle_control import control_throttle
 
 # Load environment variables from .env file
 load_dotenv()
@@ -14,5 +17,8 @@ RPI_API_URL = os.getenv('RPI_API_URL')
 def index():
     return render_template('index.html', rpi_api_url=RPI_API_URL)
 
+app.add_url_rule('/video', 'video_feed', video_feed)
+app.add_url_rule('/control/steering', 'control_steering', control_steering, methods=['POST'])
+app.add_url_rule('/control/throttle', 'control_throttle', control_throttle, methods=['POST'])
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, threaded=True)
